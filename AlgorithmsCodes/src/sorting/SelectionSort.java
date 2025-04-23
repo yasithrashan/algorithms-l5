c898a0f99e27b680b7008d35d4c3cd91457bcb76
Yasith Rashan <154666486+yasithrashan2003@users.noreply.github.com>
2025-04-23 21:01:13 +0530

    Add selection sort code

---
+++ AlgorithmsCodes/src/sorting/SelectionSort.java
@@ -0,0 +1,45 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void sort(int[] values) {
+        int lastUnsorted= values.length-1;;
+
+        while(lastUnsorted >= 0) {
+            int maxIndex=0;
+            int maxValue=values[0];
+
+            for(int i=1;i<lastUnsorted;i++) {
+                if(values[i]>maxValue) {
+                    maxIndex=i;
+                    maxValue=values[i];
+                }
+            }
+
+            // Show current max and where it's being moved
+            System.out.println("Max value " + maxValue + " found at index " +
+                    maxIndex + ", swapping with index " + lastUnsorted);
+
+            //  then swap it with the last one, and add it to the sorted section
+            values[maxIndex]=values[lastUnsorted];
+            values[lastUnsorted]=maxValue;
+
+            // Print array after each swap
+            System.out.println("Array after iteration: " + Arrays.toString(values));
+
+            lastUnsorted--;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] values={5,6,3,2,9,1,2,1};
+        System.out.println("Original array: " + Arrays.toString(values));
+        sort(values);
+        System.out.println("Sorted array: " + Arrays.toString(values));
+
+    }
+}
